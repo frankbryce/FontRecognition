42ccfdcb328398e0e4d85a442ed7dc1d9118ccc2
Frank Bryce <jonnyjack7@gmail.com>
2021-04-17 21:38:24 -0400

    Change ffn size & depth to be deeper and narrower.

--- point_ffn.py
+++ point_ffn.py
@@ -5,6 +5,7 @@ def point_wise_feed_forward_network(d_model, dff):
     return tf.keras.Sequential(
         [
             tf.keras.layers.Dense(dff, activation="relu"),  # (batch_size, seq_len, dff)
-            tf.keras.layers.Dense(d_model),  # (batch_size, seq_len, d_model)
+            tf.keras.layers.Dense(dff/2, activation="relu"),  # (batch_size, seq_len, dff)
+            tf.keras.layers.Dense(d_model, activation="relu"),  # (batch_size, seq_len, d_model)
         ]
     )
